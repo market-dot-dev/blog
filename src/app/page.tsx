264049787f5bdb69a328667f6f39c08b46b38cb9
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2024-07-17 13:59:54 -0400

    update design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
 import Container from "@/app/_components/container";
-import { HeroPost } from "@/app/_components/hero-post";
-import { Intro } from "@/app/_components/intro";
 import { RenderPosts } from "@/app/_components/render-posts";
 import { getAllPosts } from "@/lib/api";
 import Link from "next/link";
-import Image from "next/image";
-import markdownToHtml from "@/lib/markdownToHtml";
 
 
 export default async function Index() {
@@ -15,14 +11,29 @@ export default async function Index() {
     <main>
       <Container>
         <div className="flex flex-col md:flex-row">
-          {/* Left Column */}
-          <div className="md:fixed top-0 left-0 w-full md:w-2/5 h-full bg-slate-800 text-slate-50 flex flex-col justify-center p-8 lg:py-32 xl:px-32" style={{ backgroundImage: "url(/assets/blog/voronoi.png)" }}>
+          <div className="md:fixed top-0 left-0 w-full md:w-2/5 h-full bg-slate-800 text-slate-50 flex flex-col justify-center p-8 lg:py-16 xl:px-16" style={{ backgroundImage: "url(/assets/blog/voronoi.png)" }}>
             <section className="flex-col flex">
-              <Link href="/" className="text-xl md:text-2xl text-center md:text-left font-bold tracking-tight leading-tight md:pr-8 font-mono mb-2">
-                Changelog
-              </Link>
+              <div className="flex items-center mx-auto md:container"> {/* Add md:justify-start class */}
+                <div className="h-50px">
+                  <svg width="50" height="50" xmlns="http://www.w3.org/2000/svg">
+                    <circle fill="none" stroke="#22c55e" strokeWidth="2" cx="20" cy="25" r="13"> {/* Fix strokeWidth attribute */}
+                      {/* <animate attributeName="opacity" dur="3s" values="0;1;0" repeatCount="indefinite" begin="0.1" /> */}
+                    </circle>
+
+                    <circle fill="#22c55e" stroke="none" cx="20" cy="25" r="10">
+                    <animate attributeName="opacity" dur="1.4s" values="0;1;0" repeatCount="indefinite" begin="0.1" />
+                    </circle>
+                  </svg>
+                </div>
+
+                <div className="flex items-center">
+                  <Link href="/" className="text-xl md:text-2xl text-center md:text-left font-bold align-center tracking-tight leading-tight md:pr-8">
+                   Changelog
+                  </Link>
+                </div>
+              </div>
               <h4 className="text-center md:text-left text-md md:text-lg font-extralight">
-                Updates from the team building Gitwallet.
+                Updates from the team building Gitwallet. 
               </h4>
             </section>
           </div>
