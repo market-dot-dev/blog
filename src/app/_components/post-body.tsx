395685d64d1c36722b300ecbc13a3091a41ff2d2
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2025-06-21 08:11:34 -0400

    typography changes

--- src/app/_components/post-body.tsx
+++ src/app/_components/post-body.tsx
@@ -1,14 +1,15 @@
-import markdownStyles from "./markdown-styles.module.css";
-
 type Props = {
   content: string;
 };
 
 export function PostBody({ content }: Props) {
   return (
-    <div className="max-w-3xl mx-auto">
+    <div className="max-w-none">
       <div
-        className={markdownStyles["markdown"]}
+        className="prose prose-lg prose-neutral max-w-none 
+                   prose-headings:text-accent-7 
+                   prose-a:text-blue-600 prose-a:hover:text-blue-500
+                   dark:prose-invert dark:prose-headings:text-white"
         dangerouslySetInnerHTML={{ __html: content }}
       />
     </div>
